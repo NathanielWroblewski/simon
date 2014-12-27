b7a6fb42bb37f6e8c4ff0d665b6565bed9e30e54
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2014-12-27 12:20:46 -0600

    add countdown

--- public/javascripts/simon.js
+++ public/javascripts/simon.js
@@ -2,6 +2,23 @@ $(document).ready(function() {
   window.game = new Simon.Views.Game({el: '#simon'})
   window.game.setListeners()
 
-  window.game.addColor()
-  window.game.addColor()
+  setTimeout(function() {
+    $('.inner-ring').html('<h2>3</h2>')
+  }, 1000)
+
+  setTimeout(function() {
+    $('.inner-ring').html('<h2>2</h2>')
+  }, 2000)
+
+  setTimeout(function() {
+    $('.inner-ring').html('<h2>1</h2>')
+  }, 3000)
+
+  setTimeout(function() {
+    $('.inner-ring').html('')
+    window.game.addColor()
+    setTimeout(function() {
+      window.game.addColor()
+    }, 175)
+  }, 4000)
 })
